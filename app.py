9338f3f87ced241428c6d8ea0ad36bb2073e9d70
zawatan0111 <hirototanzawa@gmail.com>
2023-10-10 07:52:37 +0900

    2023/10/10 7:52 |

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html',  title="Form sample",  message="お名前は？")
+	return render_template('index.html',  title="ポイントカード照会ページ",  message="ポイントカード番号を入力")
 
 @app.route('/', methods=['POST'])
 def   form():
 	field = request.form['field']
-	return render_template('index.html', title="Form sample",  message="こんにちは、%s さん！" % field)
+	return render_template('index.html', title="%s さんのポイントカード照会ページ" % field,  message="保有ポイント : ")
 
 if __name__ == '__main__':
 	app.debug = True
