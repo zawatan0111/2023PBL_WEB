2f167c82a03280722217963b852597b4b917634c
zawatan0111 <hirototanzawa@gmail.com>
2023-10-09 17:20:33 +0900

    2023/10/9 17:20 |

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html',  title="Form sample",  message="お名前は？")
+
+@app.route('/', methods=['POST'])
+def   form():
+	field = request.form['field']
+	return render_template('index.html', title="Form sample",  message="こんにちは、%s さん！" % field)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='localhost') 
